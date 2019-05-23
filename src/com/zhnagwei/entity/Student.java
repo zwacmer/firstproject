1c12f1728f80f4fc9e7e9e38ec281a60d06a2a52
zwacmer <526388965@qq.com>
2019-05-23 21:50:55 +0800

    提交

---
+++ src/com/zhnagwei/entity/Student.java
@@ -0,0 +1,37 @@
+package com.zhnagwei.entity;
+
+public class Student {
+	private Integer id;
+	private String name;
+	private String password;
+	private String image;
+	
+	public Student() {
+		super();
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+}
